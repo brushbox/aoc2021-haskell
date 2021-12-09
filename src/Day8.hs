2a40a9c2163c5c436b45555e226ede2a37540ada
Peter Sumskas <peter.sumskas@envato.com>
2021-12-09 11:20:15 +1100

    Golfed

--- src/Day8.hs
+++ src/Day8.hs
@@ -3,7 +3,6 @@ module Day8
     , part2
     ) where
 
-import Debug.Trace
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Map.Strict (Map)
@@ -16,17 +15,11 @@ import Data.List.Split
 type Inputs = [String]
 type Outputs = [String]
 
-data Segment = A | B | C | D | E | F | G
-    deriving (Enum, Show, Ord, Eq)
-
 part1 :: IO ()
 part1 = do
     putStrLn "Day 8 part 1"
     contents <- readFile "day8.txt"
-    let displays = map parseLine $ lines contents
-    let displays' = map (\(i, o) -> (uniqueSegments i, o)) displays
-    let outputs = map (\(_, o) -> uniqueSegments o) displays
-    let answer = sum $ map length outputs
+    let answer = sum $ map (length . uniqueSegments . snd . parseLine) $ lines contents
 
     putStrLn $ show answer
 
@@ -49,149 +42,70 @@ parseLine line =
         outputs = map sort $ splitOn " " display
         [diagnostics, display] = splitOn " | "  line
 
+type Signals = Set Char
+type DigitMapping = Map Signals Int
+
 part2 :: IO ()
 part2 = do
     putStrLn "Day 8 part 2"
-    -- contents <- readFile "day8_example.txt"
     contents <- readFile "day8.txt"
     let displays = map parseLine $ lines contents
-    -- let inputs = ["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"]
-    -- let outputs = ["fdgacbe", "cefdb", "cefbgd", "gcbe"]
-    -- let mapping = inferDigitMappings inputs
-    -- let num = mappingToNumber outputs mapping
-    -- putStrLn $ show mapping
-    -- putStrLn $ show num
-
-    -- let inputs2 = ["edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec"]
-    -- let outputs2 = ["fcgedb", "cgb", "dgebacf", "gc"]
-    -- let mapping2 = inferDigitMappings inputs2
-    -- let num2 = mappingToNumber outputs2 mapping2
-    -- putStrLn $ show mapping2
-    -- putStrLn $ show num2
     let results = map (\(ins, outs) -> mappingToNumber outs (inferDigitMappings ins)) displays
-    putStrLn $ show results
     putStrLn $ show (sum results)
-
-type Input = Char
-type Mapping = (Char, Set Segment )
-type Signals = Set Char
-
-type DigitMapping = Map Signals Int
 
 mappingToNumber :: [String] -> DigitMapping -> Int
 mappingToNumber inputs mapping =
     foldl (\num signals -> num * 10 + (digit signals)) 0 inputs
     where
         digit :: String -> Int
-        -- digit signals = Map.findWithDefault -1 signals mapping
-        digit signals = fromMaybe (-1) $ Map.lookup (Set.fromList signals) mapping
+        digit signals = Map.findWithDefault (-1) (Set.fromList signals) mapping
 
 inferDigitMappings :: [String] -> DigitMapping
 inferDigitMappings unknownDigits =
-    mapTwo
+    foldl (\m (signals, val) -> insert signals val m) Map.empty (zip [zero, one, two, three, four, five, six, seven, eight, nine] [0..])
     where
-        mapOne = insert one 1 Map.empty
-        mapFour = insert four 4 mapOne
-        mapSeven = insert seven 7 mapFour
-        mapEight = insert eight 8 mapSeven
-        mapThree = insert three 3 mapEight
-        mapSix = insert six 6 mapThree
-        mapNine = insert nine 9 mapSix
-        mapZero = insert zero 0 mapNine
-        mapFive = insert five 5 mapZero
-        mapTwo = insert two 2 mapFive
-
         insert :: String -> Int -> DigitMapping -> DigitMapping
         insert signals value mp = Map.insert (Set.fromList signals) value mp
-        one :: String
-        one = findByLength 2
-        four = findByLength 4
-        seven = findByLength 3
-        eight = findByLength 7
-        three = findBySubset one fives
-        six = findByNotSubset one sixes
-        nine = findBySubset three sixes
-        -- zero = fromMaybe "" $ find (\s -> not (s `elem` [six, nine])) sixes
-        zero = fromMaybe "" $ find (not . (`elem` [six, nine])) sixes
-        five = findBySuperset six fives
-        two = fromMaybe "" $ find (not . (`elem` [three, five])) fives
 
-        fives :: [String]
-        fives = (filterByLength 5 unknownDigits)
-        sixes :: [String]
-        sixes = filterByLength 6 unknownDigits
+        zero  = findWhere (notIn [six, nine]) sixSegments
+        one   = findWhere (lengthIs 2) unknownDigits
+        two   = findWhere (notIn [three, five]) fiveSegments
+        three = findWhere (isSubset one) fiveSegments
+        four  = findWhere (lengthIs 4) unknownDigits
+        five  = findWhere (isSuperset six) fiveSegments
+        six   = findWhere (isNotSubset one) sixSegments
+        seven = findWhere (lengthIs 3) unknownDigits
+        eight = findWhere (lengthIs 7) unknownDigits
+        nine  = findWhere (isSubset three) sixSegments
 
-        findByLength :: Int -> String
-        findByLength len = fromMaybe "" $ find (\s -> length s == len) unknownDigits
-        findBySubset :: String -> [String] -> String
-        findBySubset lookFor candidates = fromMaybe "" $ find (isSubset lookFor) candidates
-        findBySuperset :: String -> [String] -> String
-        findBySuperset super candidates = fromMaybe "" $ find (\lookFor -> isSubset lookFor super) candidates
-        findByNotSubset :: String -> [String] -> String
-        findByNotSubset lookFor candidates = fromMaybe "" $ find (not . (isSubset lookFor)) candidates
-        isSubset :: String -> String -> Bool
-        isSubset lookFor within =
-            (Set.intersection lookForSet withinSet) == lookForSet
-            where
-                lookForSet = Set.fromList lookFor
-                withinSet = Set.fromList within
+        fiveSegments = filterByLength 5 unknownDigits
+        sixSegments = filterByLength 6 unknownDigits
+
+        findWhere :: (String -> Bool) -> [String] -> String
+        findWhere pred candidates =
+            case (find pred candidates) of
+                Nothing -> error "We did something wrong"
+                Just result -> result
+
 
 filterByLength :: Int -> [String] -> [String]
--- filterByLength len strs = filter (\s -> length s == len) strs
-filterByLength len strs = filter ((len ==) . length) strs
+filterByLength len = filter (lengthIs len)
 
-intersectMappings :: [Mapping] -> [Mapping] -> [Mapping]
-intersectMappings a b =
-    map intersect $ zip a b
+notIn :: [String] -> String -> Bool
+notIn candidates = not . (`elem` candidates)
+
+lengthIs :: Int -> String -> Bool
+lengthIs len = (len ==) . length
+
+isSubset :: String -> String -> Bool
+isSubset lookFor within =
+    (Set.intersection lookForSet withinSet) == lookForSet
     where
-        intersect :: (Mapping, Mapping) -> Mapping
-        -- intersect ((c, m1), (_, m2)) = traceShow (m1, m2) (c, Set.intersection m1 m2)
-        intersect ((c, m1), (_, m2)) = (c, Set.intersection m1 m2)
+        lookForSet = Set.fromList lookFor
+        withinSet = Set.fromList within
 
-initialMappings :: [Mapping]
-initialMappings = map (\c -> (c, allSegments)) "abcdefg"
+isNotSubset :: String -> String -> Bool
+isNotSubset lookFor = not . (isSubset lookFor)
 
-calcMappings :: [Char] -> [Mapping]
-calcMappings inputs =
-    map calcMapping "abcdefg"
-    where
-        calcMapping c
-            | c `elem` inputs = (c, candidates inputs)
-            | otherwise = (c, inverseCandidates inputs)
-    
--- invertInput :: [Char] -> [Char]
--- invertInput input = Set.toList $ Set.difference (Set.fromList "abcdefg") (Set.fromList input)
-
-candidates :: [Char] -> Set Segment
-candidates inputs = 
-    case (length inputs) of
-        1 -> set2
-        3 -> set3
-        4 -> set4
-        otherwise -> allSegments
-
-inverseCandidates :: [Char] -> Set Segment
-inverseCandidates inputs = 
-    case (length inputs) of
-        2 -> invertSet set2
-        3 -> invertSet set3
-        4 -> invertSet set4
-        5 -> Set.fromList [B, C, E, F]
-        6 -> Set.fromList [C, D, E]
-        otherwise -> Set.empty
-
-set2 :: Set Segment
-set2 = Set.fromList [C, F]
-
-set3 :: Set Segment
-set3 = Set.fromList [A, C, F]
-
-set4 :: Set Segment
-set4 = Set.fromList [B, C, D, F]
-
-invertSet :: Set Segment -> Set Segment
-invertSet = Set.difference allSegments
-
-allSegments :: Set Segment
-allSegments = Set.fromList [A, B, C, D, E, F, G]
-
+isSuperset :: String -> String -> Bool
+isSuperset super = ((flip isSubset) super)
