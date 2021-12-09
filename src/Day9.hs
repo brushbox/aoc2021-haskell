1126a95c7e1e5d8a66659787ca0cfdca7f809dd4
Peter Sumskas <peter.sumskas@envato.com>
2021-12-09 22:06:58 +1100

    Played some golf

--- src/Day9.hs
+++ src/Day9.hs
@@ -48,7 +48,7 @@ exploreBasin seafloor pt basinPoints =
     where
         points = filter (\pt -> notVisited pt && notPeak pt) $ quadrants pt
         notVisited pt = not (Set.member pt basinPoints)
-        notPeak (x, y) = (slookup x y seafloor) /= '9'
+        notPeak pt = (floorHeight seafloor pt) /= '9'
 
 quadrants :: Point -> [Point]
 quadrants (x, y) = [(x + dx, y + dy) | (dx, dy) <- [(0, -1), (0, 1), (1, 0), (-1, 0)]]
@@ -62,32 +62,26 @@ mapSeafloor contents =
         height = length rows
 
 lowPoints :: Seafloor -> [Point]
-lowPoints seafloor@(Seafloor width height _ ) = [(x, y) | x <- [0..(width-1)], y <- [0..(height-1)], isLower x y seafloor]
+lowPoints seafloor@(Seafloor width height _ ) = [(x, y) | x <- [0..(width-1)], y <- [0..(height-1)], isLower (x, y) seafloor]
 
-riskLevels :: [(Int, Int)] -> Seafloor -> [Int]
-riskLevels points seafloor = map (\(x, y) -> (1 + (Char.digitToInt $ slookup x y seafloor))) points
+riskLevels :: [Point] -> Seafloor -> [Int]
+riskLevels points seafloor = map (\pt -> (1 + (Char.digitToInt $ floorHeight seafloor pt))) points
 
-isLower :: Int -> Int -> Seafloor -> Bool
-isLower x y s =
-    pt < north &&
-        pt < south &&
-        pt < east &&
-        pt < west
+isLower :: Point -> Seafloor -> Bool
+isLower point s =
+    all (pt < ) heights
     where
-        pt = slookup x y s
-        north = slookup x (y - 1) s
-        south = slookup x (y + 1) s
-        east = slookup (x + 1) y s
-        west = slookup (x - 1) y s
+        heights = map (floorHeight s) $ quadrants point
+        pt = floorHeight s point
 
-slookup :: Int -> Int -> Seafloor  -> Char
-slookup x y s =
-    case (slookup' x y s) of
+floorHeight :: Seafloor -> Point -> Char
+floorHeight s point =
+    case (floorHeight' s point) of
         Nothing -> '9'
         Just c -> c
 
-slookup' :: Int -> Int -> Seafloor  -> Maybe Char
-slookup' x y (Seafloor w h map)
+floorHeight' :: Seafloor -> Point -> Maybe Char
+floorHeight' (Seafloor w h map) (x, y) 
     | x < 0 || x >= w = Nothing
     | y < 0 || y >= h = Nothing
     | otherwise = Just ((map !! y) !! x)
