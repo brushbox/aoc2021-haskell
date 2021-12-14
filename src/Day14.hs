2c3a7e4b7a1dd313dd65be7b9996545371c086c8
Peter Sumskas <peter.sumskas@envato.com>
2021-12-14 23:33:19 +1100

    Definitely the wrong approach

--- src/Day14.hs
+++ src/Day14.hs
@@ -3,73 +3,86 @@ module Day14
     , part2
     ) where
 
+import Debug.Trace
+import Data.Word
+import qualified Data.ByteString as B
+import qualified Data.Text.Encoding as E
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.List
 import Data.List.Split
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 
-type Template = String
-type PairMappings = Map (Char, Char) Char
+type Template = B.ByteString
+type Element = Word8
+type PairMappings = Map (Element, Element) Element
 
 part1 :: IO ()
 part1 = do
     putStrLn "Day 14 part 1"
-    input <- readFile "day14.txt"
+    input <- E.decodeUtf8 <$> B.readFile "day14.txt"
     -- input <- readFile "day14_example.txt"
     let (template, mapping) = parseInput input
+    let t' = applyRules mapping template
     let result = calcPolymer mapping template 10
     putStrLn $ show result
+    -- putStrLn $ show t'
 
 part2 :: IO ()
 part2 = do
     putStrLn "Day 14 part 2"
-    input <- readFile "day14.txt"
-    -- input <- readFile "day14_example.txt"
+    -- input <- E.decodeUtf8 <$> B.readFile "day14.txt"
+    input <- E.decodeUtf8 <$> B.readFile "day14_example.txt"
     let (template, mapping) = parseInput input
-    let result = calcPolymer mapping template 40
+    let result = calcPolymer mapping template 20
     putStrLn $ show result
+    -- putStrLn $ show template
 
 calcPolymer :: PairMappings -> Template -> Int -> Int
 calcPolymer mapping template count = max - min
   where
     (min, max) = minMax counts
-    counts = countLetters t'
-    t' = foldl (\t _ -> applyRules mapping t) template [1..count]
-  
-parseInput :: String -> (Template, PairMappings)
+    counts = trace "counting ..." $ countLetters t'
+    t' = foldl (\t c -> trace (show c) (applyRules mapping t)) template [1..count]
+
+parseInput :: Text -> (Template, PairMappings)
 parseInput input = (template, pairMappings)
   where
-    template = head ls
+    template = E.encodeUtf8 $ head ls
     pairMappings = foldl (\m (from, to) -> Map.insert from to m) Map.empty (parsedPairs)
     parsedPairs = map parsePair pairs
     pairs = drop 2 ls
-    ls = lines input
+    ls = T.lines input
 
-parsePair :: String -> ((Char, Char), Char)
+parsePair :: Text -> ((Element, Element), Element)
 parsePair mapping = ((first, second), insert)
   where
-    [first, second] = from
-    insert = head to
-    [from, to] = splitOn " -> " mapping
+    first = B.head (E.encodeUtf8 from)
+    second = B.head $ B.tail (E.encodeUtf8 from)
+    insert = B.head (E.encodeUtf8 to)
+    [from, to] = T.splitOn (T.pack " -> ") mapping
 
 applyRules :: PairMappings -> Template -> Template
 applyRules mappings template = template'
   where
-    template' = (head template):inserted
-    inserted = foldr (\pair template' -> prependMapping pair template') "" pairs
-    pairs = zip template (drop 1 template)
+    template' = (B.head template) `B.cons` inserted
+    inserted = foldr (\pair template' -> prependMapping pair template') B.empty pairs
+    pairs = B.zip template (B.drop 1 template)
     prependMapping pair template =
-      let [a, b] = applyMapping pair in
-          (a:b:template)
-    applyMapping pair@(_,b) = [mappings Map.! pair, b]
+      let (a, b) = applyMapping pair in
+        a `B.cons` (b `B.cons` template)
+    applyMapping pair@(_,b) = (mappings Map.! pair, b)
 
-countLetters :: String -> Map Char Int
+countLetters :: B.ByteString -> Map Element Int
 countLetters polymer =
-  foldl (\m c -> Map.insertWith (+) c 1 m) Map.empty polymer
+  trace ("Counting letters" ++ (show $ B.length polymer)) $ Map.fromListWith (+) $ (B.zipWith (\z _ -> (z, 1)) polymer polymer)
+-- countLetters polymer =
+--   trace ("Counting letters" ++ (show $ B.length polymer)) $ B.foldl (\m c -> Map.insertWith (+) c 1 m) Map.empty polymer
 
-minMax :: Map Char Int -> (Int, Int)
-minMax letterCounts = (min, max)
+minMax :: Map Element Int -> (Int, Int)
+minMax letterCounts = trace "getting minmax" (min, max)
   where
-    min = snd $ head sorted
-    max = snd $ last sorted
-    sorted = sortBy (\(_, a) (_, b) -> compare a b) $ Map.toList letterCounts
+    min = head sorted
+    max = last sorted
+    sorted = sort $ Map.elems letterCounts
